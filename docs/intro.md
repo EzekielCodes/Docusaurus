baecc50fe705b47802f056f05ca723c953fabfca
Roel Van Steenberghe <roel.vansteenberghe@vbridge.eu>
2022-04-29 14:52:17 +0200

    add initial template

---
+++ docs/intro.md
@@ -0,0 +1,33 @@
+---
+id: docs-intro
+title: Inleiding
+sidebar_position: 0
+---
+
+Inleiding
+==============
+
+Bedoeling van de workshop
+--------------
+
+
+
+Vereiste voorkennis
+-----------
+
+
+
+Tips voor je begint 
+---------------------
+
+
+
+
+Requirements
+-----------------
+
+Om deze workshop uit te voeren heb je enkele dingen nodig:
+
+* ...
+* ...
+* ...
---
+++ docusaurus.config.js
@@ -0,0 +1,97 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  plugins: [
+    require.resolve('@cmfcmf/docusaurus-search-local')
+  ],
+  title: 'Infrastructure as code',
+  tagline: 'workshop',
+  url: 'https://xxx.ikdoeict.be',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/ikdoeict.png',
+  organizationName: 'ikdoeict', // Usually your GitHub org/user name.
+  projectName: 'proejct-iac', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://gitlab.com/ikdoeict/************',
+        },
+        
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Infrastructure as code',
+        logo: {
+          alt: 'ikdoeict',
+          src: 'img/ikdoeict.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'docs-intro',
+            position: 'left',
+            label: 'Opgaves',
+          },
+          
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Workshop',
+                to: '/',
+              },
+            ],
+          },
+          
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'website',
+                to: 'https://www.odisee.be',
+              },
+              {
+                label: 'GitLab',
+                href: 'https://gitlab.com/ikdoeict',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Odisee-opleiding Elektronica-ict, built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['markdown'],
+      },
+    }),
+};
+
+module.exports=config
